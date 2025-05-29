a0518c7512aaa115495011b2ea7e05e2d409ab65
Raihan-143 <raihan172hasan@gmail.com>
2025-05-30 05:32:20 +0600

    ViewApplications section is updated to their applicant status

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,12 +1,60 @@
+import axios from 'axios';
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
 const ViewApplications = () => {
-    const {job_id}=useParams()
-    const applications=useLoaderData();
+    const { job_id } = useParams()
+    const applications = useLoaderData();
+
+    const handleStatusChange=(e, app_id)=>{
+        console.log(e.target.value,app_id);
+
+        axios.patch(`http://localhost:5000/applications/${app_id}`, {status: e.target.value})
+        .then(res=>{
+            console.log(res.data)
+            if(res.data.modifiedCount){
+                alert('Application status updated!')
+            }
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
     return (
         <div>
             <h1 className="text-3xl">{applications.length} Application for: {job_id}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Job</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            applications.map(application => <tr key={application._id}>
+                                <th>1</th>
+                                <td>{application.applicant}</td>
+                                <td>Quality Control Specialist</td>
+                                <td>
+                                    <select onChange={e=>handleStatusChange(e,application._id)} defaultValue={application.status} className="select">
+                                        <option disabled={true}>Update Status</option>
+                                        <option>Pending</option>
+                                        <option>Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
